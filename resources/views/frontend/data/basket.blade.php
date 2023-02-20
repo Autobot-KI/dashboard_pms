07f84edd85edf1132bdee093f856e1fe8dc8eca0
rhecustein <bintangwijaya18@gmail.com>
2023-02-20 14:43:08 +0700

    fix

--- resources/views/frontend/data/basket.blade.php
+++ resources/views/frontend/data/basket.blade.php
@@ -8,7 +8,7 @@
         <p>Dashboard Project 11</p>
         <h2>Basket Size</h2>
     </div>
-    <iframe title="Project 11_Perlayanan" width="100%" height="800px" src="https://app.powerbi.com/view?r=eyJrIjoiZjQ5ZWFjN2EtMDVmMC00OGU2LWEzMzItZGE4ODVlOTNjYzgzIiwidCI6ImY5ZGM2OTdmLTI4ZmUtNDU2My1hM2FhLWE2NTg2M2ZlN2YxYSIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
+    <iframe title="Project 11_Basket Size" width="100%" height="800px" src="https://app.powerbi.com/view?r=eyJrIjoiY2ZiZGVkM2MtODU0MC00YTI3LTllYTctODI2NDU4ODRiY2ZhIiwidCI6ImY5ZGM2OTdmLTI4ZmUtNDU2My1hM2FhLWE2NTg2M2ZlN2YxYSIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
     </div>
     </div>
 <!--Scroll to top-->
